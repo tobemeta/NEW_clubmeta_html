8d0fc23456bc3d2100270ce55f3bde0b7fa151cf
dodsidsyd <1991ajw@gmail.com>
2023-08-03 17:16:46 +0900

    비밀번호 변경 / 관리자 등록 수정

---
+++ admin/html-new/js/custom.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    console.log(23);
+});
+
+// confirmPop
+function conAlert(msg, callback) {
+    var $m = _createModal_Load(msg);
+    $m.find('.btn-confirm').off('click').on('click', callback).show();
+    $m.find('.modal-body').html(msg);
+    $m.modal('show');
+    $m.on('hide.bs.modal', function () {
+        $(this).remove();
+    });
+    return $m;
+}
+
+function _createModal_Load(msg) {
+    var html = '';
+    html += '<div class="modal fade" id="alertModal" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="false">';
+    html += '	<div class="modal-dialog" role="document">';
+    html += '		<div class="modal-content">';
+    html += '			<div class="modal-body">' + msg + '</div>';
+    html += '			<div class="modal-footer">';
+    html += '				<button class="btn btn-secondary btn-confirm" type="button" onclick="location.reload()">확인</button>';
+    html += '			</div>';
+    html += '		</div>';
+    html += '	</div>';
+    html += '</div>';
+    var $m = $(html);
+    $('body').append($m);
+    return $m;
+}
