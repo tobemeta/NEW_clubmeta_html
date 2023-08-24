bc22cf08c666a18cd5026b7c6f8d29b5a06f691c
dodsidsyd <1991ajw@gmail.com>
2023-08-24 10:34:22 +0900

    프로필 추가

--- front/resources/js/common.js
+++ front/resources/js/common.js
@@ -10,6 +10,7 @@ const ui = {
         _this.etc();
         _this.tooltip.init();
         _this.select.init();
+        _this.lottie();
     },
     header: () => {
         let scrolling;
@@ -761,6 +762,54 @@ const ui = {
             }
         });
         // $btnPlay.each(function () {});
+    },
+    lottie: function (readyEvt, completeEvt) {
+        const $lottie = $('[data-lottie]');
+        if (!$lottie.length) return;
+        const $lottieInit = function () {
+            $lottie.each(function () {
+                const _this = this;
+                const $this = $(this);
+                // $this.empty();
+                if (!$this.hasClass('lottie__init')) {
+                    const $data = $this.data('lottie');
+                    // $this.addClass('lottie__init').removeAttr('data-lottie').aria('hidden', true);
+                    const $loopOpt = $this.hasClass('_loop');
+                    const $stopOpt = $this.hasClass('_stop');
+                    const $autoOpt = $this.hasClass('_auto');
+                    const $sclAnimation = $this.data('animation');
+                    // let $autoplayOpt = true;
+
+                    if ($sclAnimation || $stopOpt) {
+                        $autoplayOpt = false;
+                    }
+                    const $lottieOpt = lottie.loadAnimation({
+                        container: this,
+                        renderer: 'svg',
+                        loop: $loopOpt,
+                        autoplay: $autoOpt,
+                        path: $data
+                    });
+
+                    $this.data('lottie-opt', $lottieOpt);
+
+                    // $lottieOpt.addEventListener('config_ready', function () {
+                    //     if (!!readyEvt) readyEvt(_this, $lottieOpt);
+                    // });
+
+                    // if ($loopOpt) {
+                    //     $lottieOpt.addEventListener('loopComplete', function () {
+                    //         if (!!completeEvt) completeEvt(_this, $lottieOpt);
+                    //     });
+                    // } else {
+                    //     $lottieOpt.addEventListener('complete', function () {
+                    //         if (!!completeEvt) completeEvt(_this, $lottieOpt);
+                    //     });
+                    // }
+                }
+            });
+        };
+        $lottieInit();
     }
 };
 
