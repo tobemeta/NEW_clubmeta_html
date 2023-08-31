d6f2b34072650d7083ec7096733d6648e3f07d20
EunKyoP <xoxo.popeye77@gmail.com>
2023-08-31 14:47:16 +0900

    dataTable sort

--- admin/html-new/js/demo/datatables-demo.js
+++ admin/html-new/js/demo/datatables-demo.js
@@ -1,5 +1,11 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function () {
-    if ($('#dataTable').length) $('#dataTable').DataTable();
-    if ($('#dataTable1').length) $('#dataTable1').DataTable();
+    if ($('#dataTable').length)
+        $('#dataTable').DataTable({
+            columnDefs: [{ targets: 'no-sorting', orderable: false }]
+        });
+    if ($('#dataTable1').length)
+        $('#dataTable1').DataTable({
+            columnDefs: [{ targets: 'no-sorting', orderable: false }]
+        });
 });
