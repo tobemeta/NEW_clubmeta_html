4b300cba6c00c66509b4e97333b75c5ab98941ec
EunKyoP <xoxo.popeye77@gmail.com>
2023-09-05 15:56:27 +0900

    뱃지관리, shoe/mdt관리 페이지 완료

--- admin/html-new/js/demo/datatables-demo.js
+++ admin/html-new/js/demo/datatables-demo.js
@@ -1,11 +1,5 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function () {
-    if ($('#dataTable').length)
-        $('#dataTable').DataTable({
-            columnDefs: [{ targets: 'no-sorting', orderable: false }]
-        });
-    if ($('#dataTable1').length)
-        $('#dataTable1').DataTable({
-            columnDefs: [{ targets: 'no-sorting', orderable: false }]
-        });
+    if ($('#dataTable').length) $('#dataTable').DataTable();
+    if ($('#dataTable1').length) $('#dataTable1').DataTable();
 });
