310e1eaad5c4288df6777617a28950604e540aab
dodsidsyd <1991ajw@gmail.com>
2023-09-26 14:53:16 +0900

    포인트 지급 페이지 추가

--- front/resources/js/common.js
+++ front/resources/js/common.js
@@ -11,7 +11,6 @@ const ui = {
         _this.tooltip.init();
         _this.select.init();
         _this.lottie();
-        _this.lottie2();
     },
     header: () => {
         let scrolling;
@@ -845,52 +844,52 @@ const ui = {
             });
         };
         $lottieInit();
-    },
-
-    lottie2: function () {
-        const $lottie = $('[data-lottie]');
-        if (!$lottie.length) return;
-        if (!location.host) {
-            return console.log('lottie는 서버에서만 지원됩니다.');
-        }
-        const $lottieInit = function () {
-            $lottie.each(function () {
-                const $this = $(this);
-                // $(this).empty();
-                if (!$this.hasClass('lottie__init')) {
-                    const $data = $this.data('lottie');
-                    $this.addClass('lottie__init').removeAttr('data-lottie').aria('hidden', true);
-                    const $loopOpt = $this.hasClass('_loop');
-                    const $stopOpt = $this.hasClass('_stop');
-                    const $sclAnimation = $this.data('animation');
-                    let $autoplayOpt = true;
-                    if ($sclAnimation || $stopOpt) {
-                        $autoplayOpt = false;
-                    }
-                    const $lottieOpt = lottie.loadAnimation({
-                        container: this,
-                        renderer: 'svg',
-                        loop: $loopOpt,
-                        autoplay: $autoplayOpt,
-                        path: $data
-                    });
-                    $(this).data('lottie-opt', $lottieOpt);
-                }
-            });
-        };
-        if (typeof lottie === 'undefined') {
-            let $url = '/js/lib/lottie.min.js';
-            const $path = ui.Common.getUrlPath();
-            if ($path) {
-                $url = $path + $url;
-            } else {
-                $url = '../..' + $url;
-            }
-            ui.Util.loadScript($url, $lottieInit);
-        } else {
-            $lottieInit();
-        }
     }
+
+    // lottie2: function () {
+    //     const $lottie = $('[data-lottie]');
+    //     if (!$lottie.length) return;
+    //     if (!location.host) {
+    //         return console.log('lottie는 서버에서만 지원됩니다.');
+    //     }
+    //     const $lottieInit = function () {
+    //         $lottie.each(function () {
+    //             const $this = $(this);
+    //             // $(this).empty();
+    //             if (!$this.hasClass('lottie__init')) {
+    //                 const $data = $this.data('lottie');
+    //                 $this.addClass('lottie__init').removeAttr('data-lottie').aria('hidden', true);
+    //                 const $loopOpt = $this.hasClass('_loop');
+    //                 const $stopOpt = $this.hasClass('_stop');
+    //                 const $sclAnimation = $this.data('animation');
+    //                 let $autoplayOpt = true;
+    //                 if ($sclAnimation || $stopOpt) {
+    //                     $autoplayOpt = false;
+    //                 }
+    //                 const $lottieOpt = lottie.loadAnimation({
+    //                     container: this,
+    //                     renderer: 'svg',
+    //                     loop: $loopOpt,
+    //                     autoplay: $autoplayOpt,
+    //                     path: $data
+    //                 });
+    //                 $(this).data('lottie-opt', $lottieOpt);
+    //             }
+    //         });
+    //     };
+    //     if (typeof lottie === 'undefined') {
+    //         let $url = '/js/lib/lottie.min.js';
+    //         const $path = ui.Common.getUrlPath();
+    //         if ($path) {
+    //             $url = $path + $url;
+    //         } else {
+    //             $url = '../..' + $url;
+    //         }
+    //         ui.Util.loadScript($url, $lottieInit);
+    //     } else {
+    //         $lottieInit();
+    //     }
+    // }
 };
 
 $(document).ready(() => {
